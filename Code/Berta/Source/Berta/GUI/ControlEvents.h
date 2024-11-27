36497ed288d8a19af2b047526f4e5c80f3c5b4dd
edgarbernal5 <edgar.bernal@gmail.com>
2024-11-27 17:58:15 -0300

    Fixed macro rename

--- Code/Berta/Source/Berta/GUI/ControlEvents.h
+++ Code/Berta/Source/Berta/GUI/ControlEvents.h
@@ -4,8 +4,8 @@
 * Copyright (c) 2024 Edgar Bernal (edgar.bernal@gmail.com)
 */
 
-#ifndef BT_COMMON_EVENTS_HEADER
-#define BT_COMMON_EVENTS_HEADER
+#ifndef BT_CONTROL_EVENTS_HEADER
+#define BT_CONTROL_EVENTS_HEADER
 
 #include "Berta/Core/Event.h"
 #include "Berta/Core/BasicTypes.h"
